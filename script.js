e3adb91ba62be047b0866a77522fcdbb01c13111
ExMachinery <milkcarrotc@gmail.com>
2024-07-19 01:09:06 +0500

    Add logic to create grid, using input value, but doesnt work properly

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
 const btn = document.querySelector("#btn");
 const alert = document.querySelector(".alert");
+const content = document.querySelector(".container");
 let userInput;
-// let textAlert;
 
 btn.addEventListener("click", () => {
     let userInput = prompt("Enter amount of grid you want. For 0 to 100.");
     if (userInput == null || userInput == "") {
         userInput = 0;
         alert.textContent = "Oh, you want to do nothing...";
-        // return textAlert = "Oh, you want to do nothing...";
     } else if (userInput < 0 || userInput > 100) {
         userInput = 0;
         alert.textContent = "Cant do shit with this values. Instructions unclear, right?";
-        // return textAlert = "Cant do shit with this values. Instructions unclear, right?";
     } else {
         alert.textContent = "";
-        return userInput;
+        // return userInput;
+    }
+
+    console.log(userInput);
+
+    if (userInput > 0 && userInput <= 100) {
+        for (let i = 0; i < userInput; i++) {
+            const div = document.createElement("div");
+            div.classList.toggle("square");
+            content.appendChild(div);
+        }
     }
 })
